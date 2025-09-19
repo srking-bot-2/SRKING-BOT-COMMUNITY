bba9e87b0db5194a5a77293f430dce22fdb60d11
Tahsan6532 <99996716+Tahsan6532@users.noreply.github.com>
2025-09-19 19:30:38 +0600

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস ATICK চেঞ্জ করতে পারবে🖐`}, threadID);
     }  
         }
